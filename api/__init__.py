4ad48e309d888bab45d9099a13f0467c5297b77a
tony-42069 <dsadellari@abizadvisors.com>
2024-11-26 23:42:53 -0500

    Add core configuration files

--- api/__init__.py
+++
@@ -1,8 +0,0 @@
-import azure.functions as func
-import streamlit as st
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse(
-        "This is the API endpoint for the CRE Knowledge Assistant",
-        status_code=200
-    )
--- startup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-streamlit run app/main.py --server.port 8000 --server.address 0.0.0.0
